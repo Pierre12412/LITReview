2c7375c6bad44da333ab6954d9858ef1a592fd43
Pierre12412 <pierre.bellegueule@gmx.fr>
2021-09-19 11:13:33 +0200

    css reviews

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 class BookArticle(forms.Form):
     titre = forms.CharField(max_length=20,required=True)
-    description = forms.CharField(max_length=1200, required=True)
+    description = forms.CharField(max_length=1200, required=True,widget=forms.Textarea())
     image = forms.ImageField()
 
 class ReviewForm(forms.Form):
     titre = forms.CharField(max_length=20,required=True)
     note = forms.IntegerField(max_value=5,min_value=1,required=True)
-    description = forms.CharField(max_length=1200, required=True)
+    description = forms.CharField(max_length=1200, required=True, widget=forms.Textarea())
