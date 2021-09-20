81b584f989e968bfc81585ac2d4cb35a99dfc56c
Pierre12412 <pierre.bellegueule@gmx.fr>
2021-09-20 15:51:36 +0200

    Final

--- src/critics/urls.py
+++ src/critics/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from utilisateurs.views import signup
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', signup, name='register'),
     path('',include('posts.urls')),
     path('',include('utilisateurs.urls')),
 ]
